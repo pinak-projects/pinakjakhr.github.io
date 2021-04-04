8ed26b1bbe1a236985128c0793636a8717c0f7a5
pinakjakhar <pinak@thelattice.in>
2021-04-04 19:08:39 +0530

    cahnge

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http')
 const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'application/javascript' })
+  res.writeHead(200, { 'content-type': 'application/octet-stream' })
   fs.createReadStream('index.html').pipe(res)
 })
 
