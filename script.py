42993f50051ab0e0752119dfdb78573337302a0a
Pinak Jakhar <42155101+pinakjakhr@users.noreply.github.com>
2021-04-04 18:30:52 +0530

    Add files via upload

--- script.py
+++ script.py
@@ -1,6 +1,12 @@
 import os
+from flask import Flask
+
 from http.server import HTTPServer, CGIHTTPRequestHandler
 # Make sure the server is created at current directory
+
+app = Flask(__name__)
+app.run(environ.get('PORT'))
+
 os.chdir('.')
 # Create server object listening the port 80
 server_object = HTTPServer(server_address=('', 8091), RequestHandlerClass=CGIHTTPRequestHandler)
