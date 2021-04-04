1068a451557c16dd3634080284f714e67035def4
pinakjakhar <pinak@thelattice.in>
2021-04-04 18:51:27 +0530

    changed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'content-type': 'application/javascript' })
-  fs.createReadStream('mitm.html').pipe(res)
+  fs.createReadStream('index.html').pipe(res)
 })
 
 server.listen(process.env.PORT || 3000)
